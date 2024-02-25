1e18c2e34404abe442c8b51db97abe23e119e9f0
root <root@Desktop-PC.localdomain>
2024-02-26 00:31:35 +0100

    Fix ex03

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,23 +1,3 @@
-int valide_character(char *str)
-{
-    int i;
-    int sign;
-
-    i = 0;
-    sign = 1;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')		++i;
-	while (str[i] == '-' || str[i] == '+')
-    	{
-		if (str[i] == '-')
-			sign *= -1;
-		else
-			sign *= 1;
-		++i;
-    	}
-	return (sign);
-}
-   
-
 int ft_atoi(char *str)
 {
     int i;
@@ -26,15 +6,23 @@ int ft_atoi(char *str)
 
     i = 0;
     number = 0;
-    sign = valide_character(str);
+    sign = 1;
     while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-	    ++i;
-    while (str[i] >= '0' && str[i] <= '9')
+        ++i;
+    while (str[i] == '-' || str[i] == '+')
     {
-	number = number*10 + (str[i] - '0');
+        if (str[i] == '-')
+            sign *= -1;
+        else
+            sign *= 1;
         ++i;
     }
-    number = number*sign;
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        number = number*10 + (str[i] - '0');
+        ++i;
+    }
+    number = number * sign;
     return (number);
 }
 
@@ -42,9 +30,8 @@ int ft_atoi(char *str)
 
 int main(void)
 {
-	printf("%d\n", valide_character("--+"));
-	printf("%d\n", ft_atoi("   126"));
-	printf("%d\n", ft_atoi("+126"));
+	//printf("%d\n", valide_character("---+"));
+	printf("%d\n", ft_atoi("   126xyx34"));
+	printf("%d\n", ft_atoi("-+126"));
 	printf("%d\n", ft_atoi("--+126"));
 }
-
