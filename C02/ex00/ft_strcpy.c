0fd1fad85927984ba0dee29d7d09d36d9e001c70
Marerido <144279268+Marerido@users.noreply.github.com>
2024-02-25 13:51:48 +0100

    Add files via upload

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,37 +6,20 @@
 /*   By: tunglaub <tunglaub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 08:35:27 by tunglaub          #+#    #+#             */
-/*   Updated: 2024/02/24 15:37:46 by tunglaub         ###   ########.fr       */
+/*   Updated: 2024/02/25 13:50:39 by tunglaub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *str)
+char	*ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (src[i])
 	{
-		dest[i] = str[i];
+		dest[i] = src[i];
 		++i;
 	}
 	dest[i] = '\0';
-
-	return (0);
-}
-
-#include <stdio.h>
-
-char    *ft_strcpy(char *dest, char *str);
-
-int main()
-{
-    char str[] = "Hey there";
-    char dest[0];
-
-    ft_strcpy(dest, str);
-
-    printf("%s", dest);
-
-	return (0);
+	return (dest);
 }
--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,16 +6,16 @@
 /*   By: tunglaub <tunglaub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:09:51 by tunglaub          #+#    #+#             */
-/*   Updated: 2024/02/21 16:06:24 by tunglaub         ###   ########.fr       */
+/*   Updated: 2024/02/25 13:46:33 by tunglaub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int	i;
 
 	i = 0;
-	while (i < n && src[i] == '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		++i;
