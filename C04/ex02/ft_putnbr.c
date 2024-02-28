fc4ff82f44b5d67452e773c553e15702755ea019
Marerido <144279268+Marerido@users.noreply.github.com>
2024-02-28 12:01:18 +0100

    Add files via upload
    Changes

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -1,30 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunglaub <tunglaub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 16:20:53 by tunglaub          #+#    #+#             */
+/*   Updated: 2024/02/28 11:55:17 by tunglaub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    int a;
-    int b;
+	int	a;
 
-    if (nb <  0)
-    {
-        write(1, "-", 1);
-        nb = -nb;
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
-    }
-    else if(nb > 9)
-    {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
-    }
-    else
-    {
-        a = nb + '0';
-        write(1, &a, 1);
-    }
-}
-
-int main(void)
-{
-    ft_putnbr(-6767);
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	if (nb == 2147483648)
+		write(1, "2147483648", 11);
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		a = nb + '0';
+		write(1, &a, 1);
+	}
 }
--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,37 +1,39 @@
-int ft_atoi(char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunglaub <tunglaub@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 16:24:00 by tunglaub          #+#    #+#             */
+/*   Updated: 2024/02/26 16:26:57 by tunglaub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
 {
-    int i;
-    int sign;
-    int number;
+	int	i;
+	int	sign;
+	int	number;
 
-    i = 0;
-    number = 0;
-    sign = 1;
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-        ++i;
-    while (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign *= -1;
-        else
-            sign *= 1;
-        ++i;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        number = number*10 + (str[i] - '0');
-        ++i;
-    }
-    number = number * sign;
-    return (number);
-}
-
-#include <stdio.h>
-
-int main(void)
-{
-	//printf("%d\n", valide_character("---+"));
-	printf("%d\n", ft_atoi("   126xyx34"));
-	printf("%d\n", ft_atoi("-+126"));
-	printf("%d\n", ft_atoi("--+126"));
+	i = 0;
+	number = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		++i;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		else
+			sign *= 1;
+		++i;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = number * 10 + (str[i] - '0');
+		++i;
+	}
+	number = number * sign;
+	return (number);
 }
