a767bfa84aa84504717fbdcd1a29b0e3ea3e2fed
root <root@Desktop-PC.localdomain>
2024-03-02 21:34:12 +0100

    change limits

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: tunglaub <tunglaub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:32:19 by tunglaub          #+#    #+#             */
-/*   Updated: 2024/02/27 08:59:52 by tunglaub         ###   ########.fr       */
+/*   Updated: 2024/03/02 21:32:38 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ int	ft_iterative_factorial(int nb)
 
 	i = 1;
 	number = 1;
-	if (nb == 0)
+	if (nb < 0) 
 		return (0);
+	if (nb == 1 || nb == 1)
+		return (1);
 	while (i <= nb)
 	{
 		number = number * i;
